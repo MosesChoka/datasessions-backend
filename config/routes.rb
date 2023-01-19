5fdb070c9b14805170c0037b4b311a8ddd73df1f
MC <chokamoses2@gmail.com>
2023-01-19 19:17:56 +0300

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :blogs
-
+  get 'pages/Home'
   get 'pages/About'
   get 'pages/Lessons'
   get 'pages/Podcasts'
   get 'pages/Community'
   get 'pages/Sign-Up'
+
+  root 'pages#Home'
   resources :chats
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
