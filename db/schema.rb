89f184bbbae52451c2e54e7f3da9d4116b59dd11
MC <chokamoses2@gmail.com>
2023-02-11 23:07:00 +0300

    deleted the chats scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_192523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "chats", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
