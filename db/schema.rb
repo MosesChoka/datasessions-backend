5a6dd6378cf48a75e5ae29518ebb3b2fc6de5455
MC <chokamoses2@gmail.com>
2023-03-14 22:05:07 +0300

    added jobs scaffold model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_220314) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_185718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_220314) do
   create_table "chats", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "Position"
+    t.string "Description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
